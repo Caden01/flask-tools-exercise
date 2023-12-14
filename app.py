9356769ea215dbed3b372360959c456bd3367df9
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-14 12:07:09 -0800

    Add questions with multiple choices for use to pick from and thanks the user once all questions have been asnwered

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template,  redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "secret"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -13,3 +14,43 @@ responses = []
 @app.route("/")
 def home() :
     return render_template("home.html", survey=survey)
+
+
+@app.route("/start", methods=["POST"])
+def start_survey() :
+    return redirect("/questions/0")
+
+
+@app.route("/questions/<int:question_id>")
+def show_question(question_id) :
+
+    if responses is None :
+        return redirect("/")
+
+    if len(responses) == len(survey.questions) :
+        return redirect("/complete")
+
+    if len(responses) != question_id :
+        flash("You can't skip ahead.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[question_id]
+    return render_template("question.html", question_num=question_id, question=question)
+
+
+@app.route("/answer", methods=["POST"])
+def handle_answers() :
+
+    choice = request.form["answer"]
+
+    responses.append(choice)
+
+    if len(responses) == len(survey.questions) :
+        return redirect("/complete")
+    else :
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/complete")
+def show_complete():
+    return render_template("show_complete.html")
