bafdd7fd647a1c0a8abf2630912281fa98ddfa71
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-14 10:28:02 -0800

    Add page that shows survey title and instructions with a start button

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route("/")
+def home() :
+    return render_template("home.html", survey=survey)
